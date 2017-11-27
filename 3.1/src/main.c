fdc3d059da7eef37e94bc9a717df3cf43fc9417e
OlegBEZb <olegolego@mail.ru>
2017-11-27 22:29:41 +0300

    fixed probles with more than one string

--- 3.1/src/main.c
+++ 3.1/src/main.c
@@ -11,11 +11,11 @@ int32_t strToInt( const char * str );
  
 int main(void)
 {
-	char str[]="-1<0k0k02";//enter some stroke
-	lenstr_crutch=strlen(str);
+	char str[]="12345";//enter some stroke
 	lengoogle_crutch=strlen(google);
 	volatile int32_t answer=strToInt(str);
 	
+	volatile int32_t parser_var=strToInt("-1<0k0k02");
 	while(1);
  
 	return 0;
@@ -23,26 +23,27 @@ int main(void)
 //[-99 999 999;999 999 999]
 int32_t strToInt( const char * str)
 {
-	int64_t number=0;
-	int64_t power=0;
-	for(int64_t i=lenstr_crutch-1;i>=0;i--)//reading the number backwards
-	{
-		for(int64_t j=0;j<lengoogle_crutch;j++)
-		{
-			if(str[i]==google[j])
-			{
-				if(j==10)
-				{
-					return number*(-1);//expects that the number is placed after the last sign.
-				}
-				else
-				{
-					number+=j*pow(10,power);//representing number as the summ of orders of 10 with coefficients
-					power++;
-					if(power==9) return number;//cuts leftover digits and signs)))
-				}
-			}
-		}
-	}
-	return number;
+	lenstr_crutch=strlen(str);
+    int32_t number=0;
+    int32_t power=0;
+    for(int32_t i=lenstr_crutch-1;i>=0;i--)//reading the number backwards
+    {
+        for(int32_t j=0;j<lengoogle_crutch;j++)
+        {
+            if(str[i]==google[j])
+            {
+                if(j==10)
+                {
+                    return number*(-1);//expects that the number is placed after the last sign.
+                }
+                else
+                {
+                    number+=j*pow(10,power);//representing number as the summ of orders of 10 with coefficients
+                    power++;
+                    if(power==9) return number;//cuts leftover digits and signs)))
+                }
+            }
+        }
+    }
+    return number;
 }
